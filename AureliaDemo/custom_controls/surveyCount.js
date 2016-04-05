b66820fd252a0fcb8acb07ede8931380f4c626c4
tommyly <tommyly764@gmail.com>
2016-04-05 19:54:04 +0100

    adds extra survey counter to the list control

---
+++ AureliaDemo/custom_controls/surveyCount.js
@@ -0,0 +1,15 @@
+﻿import { inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
+
+@inject(EventAggregator)
+export class SurveyCount {
+    
+    constructor(eventAggregator) {
+        this.eventAggregator = eventAggregator;
+        this.surveyCount = 0;
+
+        this.eventAggregator.subscribe('surveyCreated', survey => {
+            this.surveyCount++;
+        });
+    }
+}
